1efc547e00799a627b5130393608ae78994ac363
Gabriel Sanches <gabrielsanches131202@gmail.com>
2023-02-27 11:24:45 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/cursomc/domain/ItemPedidoPK.java
@@ -23,19 +23,24 @@ public class ItemPedidoPK implements Serializable{
 	public Pedido getPedido() {
 		return pedido;
 	}
+	
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
+	
 	public Produto getProduto() {
 		return produto;
 	}
+	
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(pedido, produto);
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
