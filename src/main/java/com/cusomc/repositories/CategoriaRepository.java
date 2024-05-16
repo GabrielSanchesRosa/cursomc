1b212b8974e3cd9784a423713b4ea11ec1a66a8e
Gabriel Sanches <gabrielsanches131202@gmail.com>
2024-05-16 09:24:02 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/cusomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.cusomc.repositories;
+
+import com.cusomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
--- src/main/java/com/cusomc/resources/CategoriaResource.java
+++ src/main/java/com/cusomc/resources/CategoriaResource.java
@@ -1,6 +1,11 @@
 package com.cusomc.resources;
 
 import com.cusomc.domain.Categoria;
+import com.cusomc.services.CategoriaService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +17,13 @@ import java.util.List;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar() {
+    @Autowired
+    private CategoriaService service;
 
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
 
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+        return ResponseEntity.ok().body(obj);
     }
 }
