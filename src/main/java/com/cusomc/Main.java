f8ff4695677d0b473fbb58ecdfa9650eba82fc48
Gabriel Sanches <gabrielsanches131202@gmail.com>
2024-05-16 09:34:20 -0300

    Criando operação de instanciação

--- src/main/java/com/cusomc/Main.java
+++ src/main/java/com/cusomc/Main.java
@@ -1,12 +1,29 @@
 package com.cusomc;
 
+import com.cusomc.domain.Categoria;
+import com.cusomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escrítorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
     }
 }
