7d1ccb17590941f890e7af2ceee487751ef51faf
Gabriel Sanches Rosa <gabrielsanches131202@gmail.com>
2024-06-21 22:12:56 -0300

    feat: Deletando uma Categoria com DELETE

--- src/main/java/com/cusomc/services/CategoriaService.java
+++ src/main/java/com/cusomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.cusomc.services;
 
 import com.cusomc.domain.Categoria;
 import com.cusomc.repositories.CategoriaRepository;
+import com.cusomc.services.exceptions.DataIntegrityException;
 import com.cusomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,5 +31,15 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+        }
     }
 }
