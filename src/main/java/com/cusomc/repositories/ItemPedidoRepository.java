7e56ea3313382eccec10ff34d374203835309a94
Gabriel Sanches Rosa <gabrielsanches131202@gmail.com>
2024-06-21 21:05:40 -0300

    feat: Implementação das classes ItemPedido e ItemPedidoPk

---
+++ src/main/java/com/cusomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.cusomc.repositories;
+
+import com.cusomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
